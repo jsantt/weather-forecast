8d6eeb2dfa7abff4414f6fbb3a9587d8d9612eaf
Jani Säntti <jani.santti@gmail.com>
2020-12-29 00:05:37 +0200

    dynamic calendar months

--- src/sections/holiday-calendar.js
+++ src/sections/holiday-calendar.js
@@ -18,51 +18,82 @@ class HolidayCalendar extends LitElement {
     return css`
       :host {
         display: block;
+
+        --day-border-radius: 8px;
       }
 
       section {
         display: grid;
         grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
         grid-template-rows: auto;
-        grid-gap: 6px;
+        grid-gap: 8px;
         align-items: start;
+
+        max-width: 300px;
       }
 
       .month {
         grid-column: span 7;
+        font-size: var(--font-size-m);
         font-weight: var(--font-weight-bold);
         margin-top: var(--space-l);
       }
 
       .weekday {
+        background: var(--color-gray-300);
+
+        font-size: var(--font-size-xs);
         text-transform: uppercase;
         margin-bottom: -1px;
       }
 
       .day {
         background: var(--color-white);
-        border-radius: var(--border-radius);
+        border-radius: var(--day-border-radius);
+
         text-align: center;
       }
 
+      .date {
+        font-weight: var(--font-weight-bold);
+        padding: var(--space-s) 0;
+      }
+
+      .holiday .date {
+        font-weight: var(--font-weight-bold);
+        border: 1px solid var(--color-blue-800);
+        border-radius: 50%;
+      }
+
       .today {
-        border: 2px solid var(--color-black);
-        margin: -2px;
+        background: var(--color-yellow-300);
+        font-weight: var(--font-weight-bold);
+      }
+
+      .past {
+        background: none;
       }
 
       .holiday {
-        background-color: var(--color-yellow-300);
         position: relative;
-        margin-bottom: 13px;
+        margin-bottom: 5px;
       }
 
       .holiday-text {
-        background-color: var(--color-yellow-300);
+        background: var(--color-white);
+        font-size: var(--font-size-xs);
         position: absolute;
         padding: 0 3px;
         border-radius: var(--border-radius);
-        right: 0;
-        top: 4px;
+        left: 0px;
+        top: -4px;
+
+        white-space: nowrap;
+      }
+
+      .sunday {
+        right: 0px;
+        left: initial;
       }
     `;
   }
@@ -80,14 +111,20 @@ class HolidayCalendar extends LitElement {
                 return html`<div
                   class="day ${day.today ? 'today' : ''} ${day.holiday
                     ? 'holiday'
-                    : ''}"
+                    : ''} ${day.past ? 'past' : ''}"
                 >
-                  <div class="weekday">${day.weekdayName}</div>
-                  <div>${day.day}</div>
-                  ${day.holiday
+                  <div class="weekday">
+                    ${day.past ? '' : day.weekdayName}
+                  </div>
+                  <div class="date">${day.day}</div>
+                  ${day.holiday !== undefined
                     ? html`<div class="holiday">
-                        <div class="holiday-text">
-                          Uudenvuodenpäivä
+                        <div
+                          class="holiday-text ${day.weekdayName === 'su'
+                            ? 'sunday'
+                            : ''}"
+                        >
+                          ${day.holiday.n}
                         </div>
                       </div>`
                     : ''}
@@ -97,9 +134,7 @@ class HolidayCalendar extends LitElement {
         </section>
 
         <div slot="footer-left"></div>
-        <div slot="footer-right">
-          Näytä koko vuosi
-        </div>
+        <div slot="footer-right"></div>
       </weather-section>
     `;
   }
@@ -107,43 +142,47 @@ class HolidayCalendar extends LitElement {
   constructor() {
     super();
 
-    // const now = new Date();
-    // const startDate = HolidayCalendar._getFirstDayOfMonth(now);
+    const now = new Date();
 
-    /* const monthAfterNext = new Date(now.setMonth(now.getMonth() + 2));
-     const endDate = HolidayCalendar._getLastDayOfMonth(
-      monthAfterNext.getMonth(),
-      monthAfterNext.getFullYear()
-    ); */
+    const months1 = [];
+    if (now.getDate() < 8) {
+      const previousMonth = HolidayCalendar._getFirstDayOfMonth(now, -1);
+      months1.push({
+        monthName: HolidayCalendar._getMonthName(previousMonth),
+        days: HolidayCalendar._dateRange(
+          previousMonth,
+          HolidayCalendar._getLastDayOfMonth(previousMonth)
+        ),
+      });
+    }
 
-    const months = [
-      {
-        monthName: 'Marraskuu',
-        days: HolidayCalendar._dateRange('2020-11-01', '2020-11-30'),
-      },
-      {
-        monthName: 'Joulukuu',
-        days: HolidayCalendar._dateRange('2020-12-01', '2020-12-31'),
-      },
-      {
-        monthName: 'Tammikuu',
-        days: HolidayCalendar._dateRange('2021-01-01', '2021-01-31'),
-      },
-    ];
+    const currentMonth = HolidayCalendar._getFirstDayOfMonth(now);
+    months1.push({
+      monthName: HolidayCalendar._getMonthName(currentMonth),
+      days: HolidayCalendar._dateRange(
+        currentMonth,
+        HolidayCalendar._getLastDayOfMonth(currentMonth)
+      ),
+    });
 
-    console.log(months);
+    if (now.getDate() >= 8) {
+      const nextMonth = HolidayCalendar._getFirstDayOfMonth(now, 1);
+      months1.push({
+        monthName: HolidayCalendar._getMonthName(nextMonth),
+        days: HolidayCalendar._dateRange(
+          nextMonth,
+          HolidayCalendar._getLastDayOfMonth(nextMonth)
+        ),
+      });
+    }
 
-    this._days = months;
+    this._days = months1;
   }
 
   static _dateRange(start, end) {
+    console.log(start, end);
     const dateArray = [];
-    const currentDate = new Date(start);
-
-    // su ma  ti  ke  to  pe  la
-    //  0 1   2   3   4   5   6
-
-    //  7 0   1   2   3   4   5
+    const currentDate = new Date(start.getTime());
 
     // if Sunday => 6, else day - 1
     const daysFromMonday =
@@ -153,26 +192,30 @@ class HolidayCalendar extends LitElement {
       dateArray.push(undefined);
     }
 
-    while (currentDate <= new Date(end)) {
+    while (currentDate.getTime() <= end.getTime()) {
       dateArray.push({
         rawDate: new Date(currentDate),
         day: currentDate.getDate(),
+        holiday: HolidayCalendar._getHoliday(currentDate),
+        past: HolidayCalendar._isPast(currentDate),
+        today: HolidayCalendar._isToday(currentDate),
         weekday: currentDate.getDay(),
         weekdayName: HolidayCalendar._getWeekdayName(currentDate),
+        weekNumber: HolidayCalendar._getWeekNumber(currentDate),
       });
-      // Use UTC date to prevent problems with time zones and DST
-      currentDate.setUTCDate(currentDate.getDate() + 1);
+
+      currentDate.setDate(currentDate.getDate() + 1);
     }
 
     return dateArray;
   }
 
-  static _getFirstDayOfMonth(date) {
-    return new Date(date.getFullYear(), date.getMonth());
+  static _getFirstDayOfMonth(date, addMonths = 0) {
+    return new Date(date.getFullYear(), date.getMonth() + addMonths);
   }
 
-  static _getLastDayOfMonth(month, year) {
-    return new Date(year, month, 0);
+  static _getLastDayOfMonth(date) {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0);
   }
 
   static _getWeekdayName(date) {
@@ -183,13 +226,42 @@ class HolidayCalendar extends LitElement {
     return date.toLocaleString('fi-FI', { month: 'long' });
   }
 
+  static _isPast(date) {
+    const now = new Date();
+    return HolidayCalendar._isToday(date) === false && date < now;
+  }
+
   static _isToday(date) {
     const now = new Date();
+
+    return HolidayCalendar._sameDay(now, date);
+  }
+
+  static _sameDay(date1, date2) {
     return (
-      date.getDate() === now.getDate() &&
-      date.getMonth() === now.getMonth() &&
-      date.getFullYear() === now.getFullYear()
+      date1.getDate() === date2.getDate() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getFullYear() === date2.getFullYear()
     );
+  }
+
+  static _getHoliday(date) {
+    return HolidayCalendar._publicHolidays().find(holiday => {
+      const hol = new Date(holiday.d);
+      return HolidayCalendar._sameDay(date, hol);
+    });
+  }
+
+  static _getWeekNumber(dt) {
+    const tdt = new Date(dt.valueOf());
+    const dayn = (dt.getDay() + 6) % 7;
+    tdt.setDate(tdt.getDate() - dayn + 3);
+    const firstThursday = tdt.valueOf();
+    tdt.setMonth(0, 1);
+    if (tdt.getDay() !== 4) {
+      tdt.setMonth(0, 1 + ((4 - tdt.getDay() + 7) % 7));
+    }
+    return 1 + Math.ceil((firstThursday - tdt) / 604800000);
   }
 
   /**
