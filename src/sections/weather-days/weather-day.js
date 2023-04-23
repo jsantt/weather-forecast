85147810cace415753668ce5a61938b540411122
Jani Säntti <jani.santti@gmail.com>
2023-04-23 10:35:48 +0300

    hide wind message when not windy

--- src/sections/weather-days/weather-day.js
+++ src/sections/weather-days/weather-day.js
@@ -201,7 +201,7 @@ class WeatherDay extends LitElement {
           </h3>
 
           <!-- headers here outside of repeat -->
-          ${windGustWarning(this.dayData).gustRating > 0
+          ${windGustWarning(this.dayData).gustRating > 1
             ? html`
                 <div class="wind_header">
                   <weather-description .dayData="${this.dayData}">
