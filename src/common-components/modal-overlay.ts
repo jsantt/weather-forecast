66915a0051e9df4443bcbfc5638ec4462865e243
Jani Säntti <jani.santti@gmail.com>
2026-04-05 17:38:26 +0300

    add preserveContent for modal

--- src/common-components/modal-overlay.ts
+++ src/common-components/modal-overlay.ts
@@ -5,11 +5,20 @@ import { customElement, property, query } from 'lit/decorators.js';
 export class ModalOverlay extends LitElement {
   @property({ type: Boolean }) open = false;
 
+  /**
+   * Preserve content in the modal when it's closed. This can be used to keep the state of the content, for example form inputs, when the modal is closed. By default, the content is removed from the DOM when the modal is closed.
+   */
+  @property({ type: Boolean }) preserveContent = false;
+
   @query('dialog') private dialog!: HTMLDialogElement;
 
   static styles = css`
     dialog {
-      border: none;s
+      background: var(--modal-overlay-background, var(--background-topmost));
+      border: none;
+      border-radius: 1.5rem;
+
+      margin-top: var(--space-l);
       z-index: var(--z-index-floating-2);
     }
 
@@ -31,9 +40,13 @@ export class ModalOverlay extends LitElement {
 
   render() {
     return html`
-      <dialog class="overlay" @close="${this.handleClose}">
-        <slot></slot>
-      </dialog>
+      ${this.open
+        ? html`<dialog class="overlay" @close="${this.handleClose}">
+            <slot></slot>
+          </dialog>`
+        : this.preserveContent
+        ? html` <slot></slot> `
+        : null}
     `;
   }
 
